0113dcaaf24f430577b23eb6285a665a489450ba
robbe Van vreckem <robbe.van.vreckem@student.ehb.be>
2021-08-16 23:10:56 +0200

    Nieuwe Commit geheel, fout in GITHUB

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 "use strict";
+const ArtikelsTonen = {
+
+Articles() {
+
+    console.log('init')
+    const filteren = document.getElementById('filterInputField').nodeValue;
+    console.log(filteren);
+    this.opstart(filteren);
+
+}
+
+}
+class artikels {
+    constructor(UUID, title, content, imageURL, likes, datum  ) {
+      this.UUID = UUID
+      this.title = title
+      this.content = content
+      this.imageURL = imageURL
+      this.likes = likes
+      this.datum = datum
+
+    }
+  }
+  
+  async function Weergeven()
+  {
+    let response = await fetch('http://thecrew.cc/news/read.php')
+    .then(response => response.json())
+    .then(data => {
+      return data.news.map(a => new artikels(a.UUID, a.title, a.content, a.imageURL, a.likes, a.datum));
+      });
+  
+      console.log(response)
+    }
+
+
+
+
+
+  
+  Weergeven();
+
+
+
